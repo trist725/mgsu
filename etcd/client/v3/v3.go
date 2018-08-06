f7f2bc918eaf8b4b81b089f7919891e067fff590
Tristone <trist725@live.cn>
2018-08-06 18:16:09 +0800

    add etcd client v3

---
+++ etcd/client/v3/v3.go
@@ -0,0 +1,192 @@
+package etcd_v3
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+const (
+	dialTimeout    = 3 * time.Second
+	requestTimeout = 3 * time.Second
+)
+
+var (
+	Endpoint string
+	gClient  *clientv3.Client
+)
+
+func Init(endpoint string) error {
+	var err error
+	gClient, err = clientv3.New(clientv3.Config{
+		Endpoints:   []string{endpoint},
+		DialTimeout: dialTimeout,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Close() {
+	gClient.Close()
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// minimum lease TTL is 5-second
+func GrantLease(timeout int64) (clientv3.LeaseID, error) {
+	resp, err := gClient.Grant(context.TODO(), timeout)
+	if err != nil {
+		return 0, err
+	}
+	return resp.ID, nil
+}
+
+//func RevokeLease(leaseID clientv3.LeaseID) (*clientv3.LeaseRevokeResponse, error) {
+//	resp, err := gClient.Revoke(context.TODO(), leaseID)
+//	return resp, err
+//}
+
+func KeepAliveLease(leaseID clientv3.LeaseID) (<-chan *clientv3.LeaseKeepAliveResponse, error) {
+	rch, err := gClient.KeepAlive(context.TODO(), leaseID)
+	return rch, err
+}
+
+//func KeepAliveLeaseOnce(leaseID clientv3.LeaseID) (*clientv3.LeaseKeepAliveResponse, error) {
+//	resp, err := gClient.KeepAliveOnce(context.TODO(), leaseID)
+//	return resp, err
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func put(key string, value string, opts ...clientv3.OpOption) (*clientv3.PutResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	resp, err := gClient.Put(ctx, key, value, opts...)
+	cancel()
+	return resp, err
+}
+
+func Put(key string, value string) error {
+	_, err := put(key, value)
+	return err
+}
+
+func PutWithLease(key string, value string, leaseID clientv3.LeaseID) error {
+	_, err := put(key, value, clientv3.WithLease(leaseID))
+	return err
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func get(key string, opts ...clientv3.OpOption) (*clientv3.GetResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	resp, err := gClient.Get(ctx, key, opts...)
+	cancel()
+	return resp, err
+}
+
+type ErrNotFoundKey struct {
+	Key string
+}
+
+func (e ErrNotFoundKey) Error() string {
+	return fmt.Sprintf("not found key=[%s]", e.Key)
+}
+
+func Get(key string) (string, error) {
+	resp, err := get(key)
+	if err != nil {
+		return "", err
+	}
+	if len(resp.Kvs) > 0 {
+		return string(resp.Kvs[0].Value), nil
+	}
+	return "", &ErrNotFoundKey{Key: key}
+}
+
+func GetWithPrefix(prefix string) (map[string]string, error) {
+	kvs := map[string]string{}
+	resp, err := get(prefix, clientv3.WithPrefix())
+	if err != nil {
+		return kvs, err
+	}
+	for _, kv := range resp.Kvs {
+		kvs[string(kv.Key)] = string(kv.Value)
+	}
+	return kvs, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func delete_(key string, opts ...clientv3.OpOption) (*clientv3.DeleteResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	resp, err := gClient.Delete(ctx, key, opts...)
+	cancel()
+	return resp, err
+}
+
+func Delete(key string) (int64, error) {
+	resp, err := delete_(key)
+	if err != nil {
+		return 0, err
+	}
+	return resp.Deleted, err
+}
+
+func DeleteWithPrefix(prefix string) (int64, error) {
+	resp, err := delete_(prefix, clientv3.WithPrefix())
+	if err != nil {
+		return 0, err
+	}
+	return resp.Deleted, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func Compact(rev int64, opts ...clientv3.CompactOption) (*clientv3.CompactResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	resp, err := gClient.Compact(ctx, rev, opts...)
+	cancel()
+	return resp, err
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func do(op clientv3.Op) (clientv3.OpResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	resp, err := gClient.Do(ctx, op)
+	cancel()
+	return resp, err
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func txn(ctx context.Context) clientv3.Txn {
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	t := gClient.Txn(ctx)
+	cancel()
+	return t
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func watch(key string, opts ...clientv3.OpOption) clientv3.WatchChan {
+	rch := gClient.Watch(context.Background(), key, opts...)
+	return rch
+}
+
+func Watch(key string) clientv3.WatchChan {
+	return watch(key)
+}
+
+func WatchWithPrefix(prefix string) clientv3.WatchChan {
+	return watch(prefix, clientv3.WithPrefix())
+}
+
+type WatchCallback func(ev *clientv3.Event)
+
+func WatchLoop(rch clientv3.WatchChan, cb WatchCallback) {
+	for {
+		select {
+		case resp := <-rch:
+			for _, ev := range resp.Events {
+				cb(ev)
+			}
+		}
+	}
+}
---
+++ etcd/client/v3/v3_test.go
@@ -0,0 +1,342 @@
+package etcd_v3
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/trist725/mgsu/util"
+	"github.com/coreos/etcd/clientv3"
+)
+
+func Test_Base(t *testing.T) {
+	endpoint := os.Getenv("ETCD_ENDPOINT")
+	if endpoint == "" {
+		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+		return
+	}
+
+	fmt.Printf("endpoint=[%s]\n", endpoint)
+
+	Init(endpoint)
+	defer Close()
+
+	key := util.GenRandomString(16)
+	value := util.GenRandomString(16)
+
+	_, err := put(key, value)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Printf("put key=[%s], value=[%s]\n", key, value)
+
+	resp, err := get(key)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for _, kv := range resp.Kvs {
+		fmt.Printf("get key=[%s], value=[%s]\n", kv.Key, kv.Value)
+	}
+
+	if _, err := delete_(key); err != nil {
+		t.Error(err)
+	} else {
+		fmt.Printf("delete key=[%s]\n", key)
+	}
+
+}
+
+func Test_GetWithRev(t *testing.T) {
+	endpoint := os.Getenv("ETCD_ENDPOINT")
+	if endpoint == "" {
+		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+		return
+	}
+
+	fmt.Printf("endpoint=[%s]\n", endpoint)
+
+	Init(endpoint)
+	defer Close()
+
+	presp, err := gClient.Put(context.TODO(), "foo", "bar1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(presp.Header.Revision)
+	_, err = gClient.Put(context.TODO(), "foo", "bar2")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	resp, err := gClient.Get(ctx, "foo", clientv3.WithRev(presp.Header.Revision))
+	cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ev := range resp.Kvs {
+		fmt.Printf("%s : %s\n", ev.Key, ev.Value)
+	}
+}
+
+func Test_PutGetDelete(t *testing.T) {
+	endpoint := os.Getenv("ETCD_ENDPOINT")
+	if endpoint == "" {
+		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+		return
+	}
+
+	fmt.Printf("endpoint=[%s]\n", endpoint)
+
+	if err := Init(endpoint); err != nil {
+		t.Error(err)
+		return
+	}
+	defer Close()
+
+	begin := time.Now()
+
+	k := "test:key:1:2:3"
+
+	for i := 0; i < 1000; i++ {
+		v := util.GenRandomString(8)
+
+		err := Put(k, v)
+
+		v, err = Get(k)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		//fmt.Println(v)
+
+		_, err = Delete(k)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		//fmt.Println(n)
+	}
+
+	fmt.Printf("consume=%v\n", time.Now().Sub(begin))
+}
+
+func Test_PutGetDeleteWithPrefix(t *testing.T) {
+	endpoint := os.Getenv("ETCD_ENDPOINT")
+	if endpoint == "" {
+		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+		return
+	}
+
+	fmt.Printf("endpoint=[%s]\n", endpoint)
+
+	Init(endpoint)
+	defer Close()
+
+	for i := range make([]int, 3) {
+		err := Put(fmt.Sprintf("key_%d", i), fmt.Sprintf("value_%d", i))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	kvs, err := GetWithPrefix("key_")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(kvs)
+
+	n, err := DeleteWithPrefix("key_")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(n)
+}
+
+func Test_Watch(t *testing.T) {
+	endpoint := os.Getenv("ETCD_ENDPOINT")
+	if endpoint == "" {
+		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+		return
+	}
+
+	fmt.Printf("endpoint=[%s]\n", endpoint)
+
+	Init(endpoint)
+	defer Close()
+
+	k := "key"
+	//v := "value"
+
+	rch := WatchWithPrefix(k)
+	go WatchLoop(rch, func(ev *clientv3.Event) {
+		fmt.Printf("%v\n", ev)
+	})
+
+	time.Sleep(1 * time.Second)
+
+	err := Put(k, util.GenRandomString(8))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	_, err = Delete(k)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	time.Sleep(5 * time.Second)
+}
+
+func Test_PutWithLease(t *testing.T) {
+	endpoint := os.Getenv("ETCD_ENDPOINT")
+	if endpoint == "" {
+		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+		return
+	}
+
+	fmt.Printf("endpoint=[%s]\n", endpoint)
+
+	Init(endpoint)
+	defer Close()
+
+	leaseID, err := GrantLease(5)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	k := "key"
+	v := "value"
+
+	err = PutWithLease(k, v, leaseID)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func Test_KeepAlive(t *testing.T) {
+	endpoint := os.Getenv("ETCD_ENDPOINT")
+	if endpoint == "" {
+		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+		return
+	}
+
+	fmt.Printf("endpoint=[%s]\n", endpoint)
+
+	Init(endpoint)
+	defer Close()
+
+	k := "foo"
+	v := "bar"
+
+	leaseID, err := GrantLease(5)
+
+	err = PutWithLease(k, v, leaseID)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// the key 'foo' will be kept forever
+	ch, err := KeepAliveLease(leaseID)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	ka := <-ch
+	fmt.Println("ttl:", ka.TTL)
+
+	time.Sleep(5 * time.Second)
+}
+
+//func Test_KeepAliveOnce(t *testing.T) {
+//	endpoint := os.Getenv("ETCD_ENDPOINT")
+//	if endpoint == "" {
+//		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+//		return
+//	}
+//
+//	fmt.Printf("endpoint=[%s]\n", endpoint)
+//
+//	Init(endpoint)
+//	defer Close()
+//
+//	k := "foo"
+//	v := "bar"
+//
+//	leaseID, err := GrantLease(5)
+//
+//	err = PutWithLease(k, v, leaseID)
+//	if err != nil {
+//		t.Error(err)
+//		return
+//	}
+//
+//	time.Sleep(4 * time.Second)
+//
+//	// to renew the lease only once
+//	resp, err := KeepAliveLeaseOnce(leaseID)
+//	if err != nil {
+//		t.Error(err)
+//		return
+//	}
+//
+//	fmt.Println("ttl:", resp.TTL)
+//}
+
+func Test_Txn(t *testing.T) {
+	endpoint := os.Getenv("ETCD_ENDPOINT")
+	if endpoint == "" {
+		t.Error("not found env ETCD_ENDPOINT or ETCD_ENDPOINT is empty string")
+		return
+	}
+
+	fmt.Printf("endpoint=[%s]\n", endpoint)
+
+	Init(endpoint)
+	defer Close()
+
+	kvc := clientv3.NewKV(gClient)
+
+	_, err := kvc.Put(context.TODO(), "key", "xyz")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	_, err = kvc.Txn(ctx).
+		If(clientv3.Compare(clientv3.Value("key"), ">", "abc")). // txn value comparisons are lexical
+		Then(clientv3.OpPut("key", "XYZ")).                      // this runs, since 'xyz' > 'abc'
+		Else(clientv3.OpPut("key", "ABC")).
+		Commit()
+	cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := kvc.Get(context.TODO(), "key")
+	cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ev := range resp.Kvs {
+		fmt.Printf("%s : %s\n", ev.Key, ev.Value)
+	}
+	// Output: key : XYZ
+}
