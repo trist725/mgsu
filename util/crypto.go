a22843dc613c23f84b697ec1410026a233f17b53
Tristone Chen <trist725@live.cn>
2019-04-26 09:44:52 +0800

    update rsa

--- util/crypto.go
+++ util/crypto.go
@@ -2,6 +2,11 @@ package util
 
 import (
 	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"sync"
 )
@@ -31,4 +36,31 @@ func MD5Sumf(format string, args ...interface{}) (output []byte) {
 	output = md5Hasher.Sum(input)
 
 	return
+}
+
+// rsa公钥加密
+func RsaPubEncrypt(publicKey []byte, origData []byte) ([]byte, error) {
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		return nil, errors.New("public key error")
+	}
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	return rsa.EncryptPKCS1v15(rand.Reader, pub, origData)
+}
+
+// rsa私钥解密
+func RsaPrivDecrypt(privateKey []byte, origData []byte) ([]byte, error) {
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		return nil, errors.New("private key error!")
+	}
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return rsa.DecryptPKCS1v15(rand.Reader, priv, origData)
 }
