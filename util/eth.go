2efa1fb618d4234b187980a460a7df8519f43a7b
Tristone <trist725@live.cn>
2022-12-15 12:28:36 +0800

    添加eth验签方法

---
+++ util/eth.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// SignVerify 验证以太坊签名
+// addr: 钱包地址Hex
+// signedMsg: 用私钥对原始数据签名后的密文Hex
+// message: 原始数据
+func SignVerify(addrHex, signedHex, message string) bool {
+	signed, err := hexutil.Decode(signedHex)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	if signed[64] != 1 && signed[64] != 0 && signed[64] != 27 && signed[64] != 28 {
+		log.Println("signedHex Decode error.")
+		return false
+	}
+
+	if signed[64] != 1 && signed[64] != 0 {
+		signed[64] -= 27
+	}
+
+	pubKey, errS := crypto.SigToPub(crypto.Keccak256([]byte(message)), signed)
+	if err != nil {
+		log.Println(errS)
+		return false
+	}
+
+	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
+	return addrHex == recoveredAddr.Hex()
+}
--- util/go.mod
+++ util/go.mod
@@ -2,4 +2,17 @@ module github.com/trist725/mgsu/util
 
 go 1.19
 
-require github.com/tealeg/xlsx v1.0.5
+require (
+	github.com/ethereum/go-ethereum v1.10.26
+	github.com/tealeg/xlsx v1.0.5
+)
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
