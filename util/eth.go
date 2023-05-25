b0fe8f7a83ed110e4247ccf8b721c4f8925639cb
Tristone <trist725@live.cn>
2023-05-25 11:28:29 +0800

    add eip191 func

--- util/eth.go
+++ util/eth.go
@@ -1,13 +1,16 @@
 package util
 
 import (
+	"crypto/ecdsa"
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-// SignVerify 验证以太坊签名
+// SignVerify 验证以太坊签名 eth_sign
 // addrHex: 钱包地址Hex
 // signedHex: 用私钥对原始数据签名后的密文Hex
 // message: 原始数据
@@ -34,4 +37,53 @@ func SignVerify(addrHex, signedHex, message string) bool {
 
 	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
 	return addrHex == recoveredAddr.Hex()
+}
+
+// SignVerify191 验证EIP191签名  personal_sign
+func SignVerify191(addrHex, signedHex, message string) bool {
+	signed, err := hexutil.Decode(signedHex)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	if signed[64] < 27 {
+		if !(signed[64] == 0 || signed[64] == 1) {
+			log.Println("Invalid last byte")
+			return false
+		}
+	} else {
+		signed[64] -= 27 // shift byte?
+	}
+
+	fullMessage := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(message), message)
+	hash := crypto.Keccak256Hash([]byte(fullMessage))
+
+	recoveredPublicKey, err := crypto.Ecrecover(hash.Bytes(), signed)
+	if err != nil {
+		log.Printf("Ecrecover failed:%s\n", err.Error())
+		return false
+	}
+	secp256k1RecoveredPublicKey, err := crypto.UnmarshalPubkey(recoveredPublicKey)
+	if err != nil {
+		log.Printf("UnmarshalPubkey failed:%s\n", err.Error())
+		return false
+	}
+
+	recoveredAddr := crypto.PubkeyToAddress(*secp256k1RecoveredPublicKey).Hex()
+
+	return strings.EqualFold(addrHex, recoveredAddr)
+}
+
+// PersonalSign EIP191签名
+func PersonalSign(message string, privateKey *ecdsa.PrivateKey) (string, error) {
+	fullMessage := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(message), message)
+	hash := crypto.Keccak256Hash([]byte(fullMessage))
+	signatureBytes, err := crypto.Sign(hash.Bytes(), privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	signatureBytes[64] += 27
+	return hexutil.Encode(signatureBytes), nil
 }
