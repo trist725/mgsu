4683c4691c45d1ebe37de7116683ea089bb69438
Tristone <trist725@live.cn>
2024-01-09 15:12:26 +0800

    添加一些字符串转换方法

--- util/string.go
+++ util/string.go
@@ -76,3 +76,74 @@ func FormatFieldName(raw string) (name string) {
 
 	return
 }
+
+func StringToInt(s string) (int, error) {
+	temp, err := strconv.ParseInt(s, 10, 0)
+	return int(temp), err
+}
+
+func StringToInt64(s string) (int64, error) {
+	return strconv.ParseInt(s, 10, 64)
+}
+
+func StringToInt32(s string) (int32, error) {
+	temp, err := strconv.ParseInt(s, 10, 32)
+	return int32(temp), err
+}
+
+func StringToint16(s string) (int16, error) {
+	temp, err := strconv.ParseInt(s, 10, 16)
+	return int16(temp), err
+}
+
+func StringToInt8(s string) (int8, error) {
+	temp, err := strconv.ParseInt(s, 10, 8)
+	return int8(temp), err
+}
+
+func StringToUint(s string) (uint, error) {
+	temp, err := strconv.ParseUint(s, 10, 0)
+	return uint(temp), err
+}
+
+func StringToUint64(s string) (uint64, error) {
+	return strconv.ParseUint(s, 10, 64)
+}
+
+func StringToUint32(s string) (uint32, error) {
+	temp, err := strconv.ParseUint(s, 10, 32)
+	return uint32(temp), err
+}
+
+func StringToUint16(s string) (uint16, error) {
+	temp, err := strconv.ParseUint(s, 10, 16)
+	return uint16(temp), err
+}
+
+func StringToUint8(s string) (uint8, error) {
+	temp, err := strconv.ParseUint(s, 10, 8)
+	return uint8(temp), err
+}
+
+func StringToFloat64(s string) (float64, error) {
+	return strconv.ParseFloat(s, 64)
+}
+
+func StringToFloat32(s string) (float32, error) {
+	temp, err := strconv.ParseFloat(s, 32)
+	return float32(temp), err
+}
+
+func StringToBool(s string) (bool, error) {
+	temp, err := strconv.ParseBool(s)
+	return temp, err
+}
+
+// StringToTime 从字符串解析出时间, 格式为: 2006-01-02 15:04:05
+func StringToTime(s string) (time.Time, error) {
+	return time.Parse("2006-01-02 15:04:05", s)
+}
+
+func StringToBytes(s string) []byte {
+	return []byte(s)
+}
