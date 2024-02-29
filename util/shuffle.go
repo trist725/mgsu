37a62df598250d757d6dc2197fc9c9832bce9733
trist <trist725@live.cn>
2024-02-29 17:16:27 +0800

    泛型洗牌

--- util/shuffle.go
+++ util/shuffle.go
@@ -1,10 +1,10 @@
 package util
 
-//Knuth-Durstenfeld Shuffle
-//时间复杂度为O(n)
-//空间复杂度为O(1)
-//必须知道数组长度
-func KnuthDurstenfeldShuffle(arr []int) []int {
+// KnuthDurstenfeldShuffle 泛型洗牌
+// 时间复杂度为O(n)
+// 空间复杂度为O(1)
+// 必须知道数组长度
+func KnuthDurstenfeldShuffle[T any](arr []T) []T {
 	for i := len(arr) - 1; i >= 0; i-- {
 		p := RandomInt64(0, int64(i))
 		a := arr[i]
